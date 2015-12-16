fc1dc93bbcdbc9c272e8f82fa9abd9b446000a2b
Victor Melo <victorhcmelo@gmail.com>
2015-12-16 15:03:13 -0200

    found how to broadcast the vectors

--- dpp.py
+++ dpp.py
@@ -3,11 +3,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import logging
+import sys, os
 
 class DPP:
     def __init__(self, grid_points, sigma_ = 0.1):
         """
-        Implements simple grid points version
+        Implements simple grid points using Gaussian kernel
         """
         self.n = len(grid_points)
         self.elements = range(0, self.n*self.n)
@@ -76,12 +77,55 @@ class DPP:
             
         return map(int, sampled)
     
-    def decompose_kernel(self):
+    def sample_dpp(self, k = None):
+        logging.basicConfig(level=logging.DEBUG, format='%(name)s (%(levelname)s): %(message)s')
+        module = sys.modules['__main__'].__file__
+        log = logging.getLogger(module)
+
+        # decomposes kernel
         D, V = np.linalg.eig(self.L) # D: eigenvalues, V: eigenvectors
-        pass
-    
-    def sample_dpp(self, k):
-        pass
+
+        if k == None:
+            # choose eigenvectors randomly
+            D = D / (1 + D)
+            v_idx = np.where(np.random.rand(len(D)) <= D)[0]
+        else:
+            # k-DPP
+            print "not implemented yet"
+            sys.exit(-1)
+
+        k = len(v_idx)
+        V = V[:,v_idx]
+
+        Y = np.zeros(k)
+        for i in xrange(k-1, -1, -1): # from k-1 until 0
+
+            # compute probabilities for each item
+            P = np.sum(V**2, axis = 1)
+            P = P / np.sum(P)
+            log.debug("P:\n%s", P)
+
+            # choose a new item to include
+            Y[i] = np.nonzero(np.random.rand(1) <= np.cumsum(P))[0][0] # takes the first non-zero element
+            log.debug("cumsum P: %s", np.cumsum(P))
+            log.debug("Y: %s", Y)
+
+            # choose a vector to eliminate
+            j = np.nonzero( V[Y[i],:] )[0][0]
+            log.debug( "js (idx vec to eliminate) = %s", np.nonzero(V[Y[i],:]) )
+            Vj = V[:,j]
+            V = np.delete(V, j, 1)
+            log.debug("j: %s, Vj: %s, V: %s", j, Vj.shape, V.shape)
+
+            # update V
+            log.debug("Shapes => Vj %s, V[Y[i],:] %s", Vj.shape, V[ Y[i],: ].shape)
+            # Vj = Vj[:,np.newaxis]         # Vj must be a column vector, eg. (100, 1)
+            # V_Yi = V[Y[i], np.newaxis, :] # V at Y[i] must be row-vector, eg. (36, 1)
+            log.debug(Vj[:,np.newaxis].shape)
+            log.debug(V[ Y[i], np.newaxis, : ].shape)
+            log.debug((Vj[:, np.newaxis] * V[ Y[i], np.newaxis, : ]).shape)
+            V = V - (Vj * V[ Y[i],: ]) / Vj[ Y[i] ]
+
 
 
 if __name__ == "__main__":
@@ -108,5 +152,8 @@ if __name__ == "__main__":
     print "B = {2} | A = {0,1}:", dpp_grid.cond_prob([2], [0,1])
     print "B = {3} | A = {0,1}:", dpp_grid.cond_prob([3], [0,1])
     print "B = {99} | A = {0,50}:", dpp_grid.cond_prob([99], [0,50])
+    # p.s: you need to convert idx_to_point to see which point it is
+
+    dpp_grid.sample_dpp()
 
 
