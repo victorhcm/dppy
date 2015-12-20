5fd6817ea8bcf5ca41c85f9820acb7f1f6c85447
Victor Melo <victorhcmelo@gmail.com>
2015-12-20 13:23:42 -0200

    Adds the Metropolis-Hastings algorithm
    This Metropolis-Hastings version still is the slowest version, since it
    must compute the determinant every time. Still faster than the algorithm
    described by Kulesza et al.
    
    In the next steps, I'll change the MH algorithm to compute the
    probabilities using the inverse, which is updated iteratively so it's
    not computationally expensive.

--- dpp.py
+++ dpp.py
@@ -3,15 +3,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import logging
-import sys, os
+import sys, os, math
 
 class DPP:
     def __init__(self, grid_points, sigma_ = 0.1):
         """
-        Implements simple grid points using Gaussian kernel
+        Implements simple grid points using a Gaussian kernel
         """
         self.n = len(grid_points)
-        self.elements = range(0, self.n*self.n)
+        self.itemset = range(0, self.n*self.n)
+
+        # parameters
         self.sigma = sigma_
         
         # create L-ensemble
@@ -55,7 +57,7 @@ class DPP:
         return conj / float(norm)
     
     def dummy_sampling(self, num_samples, verbose = False):      
-        Y = list(self.elements)
+        Y = list(self.itemset)
         sampled = []
         for it in xrange(num_samples):
             if verbose: print "Iteration: %d" % it
@@ -137,11 +139,82 @@ class DPP:
 
         return sorted(map(int, Y))
 
-    def gibbs_sampler(self):
-        pass
+    def mh_sampler(self, epsilon=0.01):
+        """
+        Metropolis-Hastings algorithm as proposed by [Fast Determinantal Point
+        Process Sampling with Application to Clustering][1]
+
+        Inputs:
+        - epsilon: parameter used to determine the number of iterations. Authors
+                   used 0.01
+
+        Outputs:
+        - Y: list of samples from the itemset
+
+
+        [1]: http://papers.nips.cc/paper/\
+            5008-fast-determinantal-point-process-\
+            sampling-with-application-to-clustering.pdf
+        """
+        logging.basicConfig(level=logging.DEBUG, format='%(name)s (%(levelname)s): %(message)s')
+        log = logging.getLogger()
+
+        # randomly initialize state $Y \subseteq S$.  it's recommended to
+        # initialize with a small size, as $o(n^{1/3})$ to avoid initial
+        # expensive inverse computations.
+        ini_sample_sz = self.n**(float(1)/3)
+        Y = list(np.random.choice(self.itemset, ini_sample_sz, replace=False))
+        log.debug("Initial sample size: %s", ini_sample_sz)
+        log.debug("Sampled elements: %s", Y)
+
+        # mixing time is $O(n log(n/epsilon)$
+        niter = int(self.n * math.log(self.n / epsilon))
+        log.debug("Number of iterations: %s", niter)
+
+        for i in xrange(niter):
+            u = np.random.choice(self.itemset, 1, replace=False)
+
+            # temporary lists to compute the probabilities
+            Ypu = list(Y); Ynu = list(Y)
+            if u not in Y:
+                Ypu.append(u)
+            else:
+                Ynu.remove(u)
+
+            log.debug("Y:\t%s", Y)
+            log.debug("Ypu:\t%s", Ypu)
+            log.debug("Ynu:\t%s", Ynu)
+
+            # probabilities of inclusion/removal of item u
+            prob_Y = self.prob(Y)
+            pu_pos = min(1, self.prob(Ypu) / prob_Y)
+            pu_neg = min(1, self.prob(Ynu) / prob_Y)
+
+            log.debug("pu_pos:\t%s", pu_pos)
+            log.debug("pu_neg:\t%s", pu_neg)
+
+            # TODO check if rand(1) >= pu_pos is correct
+            if u not in Y:
+                # includes u with probability pu_pos
+                add_elem = np.random.rand(1) <= pu_pos
+                if add_elem: 
+                    previous_sz = len(Y)
+                    Y.append(int(u))
+                    log.debug('adding elem u: %s. '
+                              'Y: %s to %s elements', u, previous_sz, len(Y))
+            else:
+                # removes u with probability pu_neg
+                rem_elem = np.random.rand(1) <= pu_neg
+                if rem_elem:
+                    previous_sz = len(Y)
+                    Y.remove(int(u))
+                    log.debug('removing elem u: %s. '
+                              'Y: %s to %s elements', u, previous_sz, len(Y))
+
+        return Y
 
 if __name__ == "__main__":
-    n = 60
+    n = 10
     grid_points = np.arange(n) / float(n)
     dpp_grid = DPP(grid_points)
 
@@ -166,10 +239,20 @@ if __name__ == "__main__":
     print "B = {99} | A = {0,50}:", dpp_grid.cond_prob([99], [0,50])
     # p.s: you need to convert idx_to_point to see which point it is
 
+    # sampling only 10 elements
     sampled_idxs = dpp_grid.sample_dpp()
     sampled_points = dpp_grid.idx_to_point[sampled_idxs]
-    print('samples: {}'.format(sampled_points))
+    plt.scatter(sampled_points[:,0], sampled_points[:,1])
+    plt.show()
 
+    # metropolis-hastings sampling is much faster, so I can quickly sample 100
+    # elements
+    n = 100
+    grid_points = np.arange(n) / float(n)
+    dpp_grid = DPP(grid_points)
+
+    sampled_idxs = dpp_grid.mh_sampler()
+    sampled_points = dpp_grid.idx_to_point[sampled_idxs]
     plt.scatter(sampled_points[:,0], sampled_points[:,1])
     plt.show()
 
